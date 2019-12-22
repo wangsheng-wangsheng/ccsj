8fc0a483b7a24d89b7b90933efb66af06c84602d
WangSheng <1251514221@qq.com>
2019-12-22 17:25:49 +0800

    人事管理系统前台空项目

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {LoginComponent} from './login/login.component';
+import {LogoutComponent} from './login/logout.component';
+import { IndexComponent } from './index/index.component';
+import { SuperAdminComponent } from './super-admin/super-admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LogoutComponent,
+    IndexComponent,
+    SuperAdminComponent
   ],
   imports: [
     BrowserModule
